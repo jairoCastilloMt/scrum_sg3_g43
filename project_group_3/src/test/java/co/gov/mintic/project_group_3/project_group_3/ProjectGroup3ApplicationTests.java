64ec963d6e7759d72c375dbfb7f04bc4b7cac392
jairoCastilloMt <jairo.castilo.mt@correo.usa.edu.co>
2021-11-22 20:12:26 -0500

    carpeta con el proyecto

---
+++ project_group_3/src/test/java/co/gov/mintic/project_group_3/project_group_3/ProjectGroup3ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.gov.mintic.project_group_3.project_group_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGroup3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
