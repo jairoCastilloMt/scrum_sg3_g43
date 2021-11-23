4f7784a12c127e19686de43165276795d374f2ff
jairoCastilloMt <jairo.castilo.mt@correo.usa.edu.co>
2021-11-22 19:38:38 -0500

    Carpetas necesarias

---
+++ back-end/project_group_3/src/main/java/co/gov/mintic/project_group_3/project_group_3/ProjectGroup3Application.java
@@ -0,0 +1,13 @@
+package co.gov.mintic.project_group_3.project_group_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGroup3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGroup3Application.class, args);
+	}
+
+}
